f9f2a25a2e8b1a40c8c94c4e452be7c47b1a7a83
Binayak <binayakd86@gmail.com>
2024-11-18 16:50:56 +0800

    initial project setup using uv

---
+++ hello.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pyiceberg-file-registration!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ permissions-fix.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# runs this script if you are having permissions issues.
+
+# change permission of workspace files and local-data folder 
+# to be accessible in the container
+chmod 777 -R ./local-data
